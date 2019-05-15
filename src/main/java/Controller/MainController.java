e43a6b873ff0807f486ac0e23d880c428f54940f
Arthur Busuttil <arthur.busuttil@epita.fr>
2019-05-15 17:03:29 +0200

    [LISTENER][MOUSE] Add Listener on MapPanel to get mouse position

--- src/main/java/Controller/MainController.java
+++ src/main/java/Controller/MainController.java
@@ -3,8 +3,7 @@ package Controller;
 import Editor.Editor;
 import Model.Editor.EditorState;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class MainController {
     Editor editor;
@@ -18,6 +17,11 @@ public class MainController {
         editor.tmpButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent actionEvent) {
                 editorState.tmpAction("J'ai click");
+            }
+        });
+        editor.mapPane.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                System.out.println("X: " + e.getX() / 16 + ", Y: " + e.getY() / 16);
             }
         });
     }
