b721b6ecf12924aac574c73198c47fd002567f98
Thomas Lupin <thomas.lupin@epita.fr>
2019-05-16 17:06:56 +0200

    [JSON] First json serialization test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,26 @@
 import Controller.MainController;
+import Model.World.World;
+import com.google.gson.Gson;
+
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         new MainController();
+
+        World world = new World();
+
+        Gson gson = new Gson();
+        String res = gson.toJson(world);
+
+        try {
+            File file = new File("test.wrld");
+            file.createNewFile();
+            FileOutputStream writer = new FileOutputStream(file);
+            writer.write(res.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
