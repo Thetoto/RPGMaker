db33fba95cb86b8af5679d011b9c15b1d3e07e7a
Arthur Busuttil <arthur.busuttil@epita.fr>
2019-06-07 14:16:00 +0200

    [TEST][TIMER] Add test for Timer

---
+++ src/test/java/TimerTest.java
@@ -0,0 +1,47 @@
+import Model.Engine.Timer;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+public class TimerTest {
+
+    Timer timer = null;
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() {
+        System.setOut(new PrintStream(out));
+        timer = new Timer(3000, actionEvent -> {
+            System.out.print("Done");
+            return;
+        });
+    }
+
+    @After
+    public void clean() {
+        System.setOut(null);
+    }
+
+    @Test
+    public void PauseTest() {
+        try {
+            timer.start();
+            Thread.sleep(2000);
+            timer.pause();
+            Thread.sleep(3000);
+            Assert.assertTrue(out.toString() + "END", out.toString().equals(""));
+            timer.resume();
+            Thread.sleep(2000);
+            Assert.assertTrue(out.toString() + "END", out.toString().equals("Done"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.assertTrue( false);
+        }
+    }
+
+}
